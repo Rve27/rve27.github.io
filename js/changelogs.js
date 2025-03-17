e8af38df70f340b6e607c24c91fe543d30251848
Rve27 <rve27github@gmail.com>
2025-03-18 00:08:28 +0700

    Add AxionOS beryllium changelog
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- js/changelogs.js
+++ js/changelogs.js
@@ -42,3 +42,18 @@ document.addEventListener('DOMContentLoaded', function() {
             changelogElement.innerHTML = '<p>Error fetching the changelog.</p>';
         });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const changelogElement = document.getElementById("axion-beryllium-changelog");
+    const url = "https://raw.githubusercontent.com/Rve27/axion-beryllium/refs/heads/main/changelogs.md";
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            const htmlContent = marked.parse(data);
+            changelogElement.innerHTML = htmlContent;
+        })
+        .catch(error => {
+            console.error('Error fetching the changelog:', error);
+            changelogElement.innerHTML = '<p>Error fetching the changelog.</p>';
+        });
+});
