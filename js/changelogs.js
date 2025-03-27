8aa3dcbbef6d27e2c6e098e207bc300afabeeb6a
Rve27 <rve27github@gmail.com>
2025-03-27 07:38:53 +0700

    Add HorizonDroid beryllium changelogs
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- js/changelogs.js
+++ js/changelogs.js
@@ -3,7 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
         { id: "rvkernel-mi8937-changelog", url: "https://raw.githubusercontent.com/Rv-Project/RvKernel-Changelogs/refs/heads/main/Mi8937.md" },
         { id: "rvkernel-mi845-changelog", url: "https://raw.githubusercontent.com/Rv-Project/RvKernel-Changelogs/refs/heads/main/Mi845.md" },
         { id: "rvkernel-alioth-changelog", url: "https://raw.githubusercontent.com/Rv-Project/RvKernel-Changelogs/refs/heads/main/alioth.md" },
-        { id: "axion-beryllium-changelog", url: "https://raw.githubusercontent.com/Rve27/beryllium/refs/heads/main/axion-changelogs.md" }
+        { id: "axion-beryllium-changelog", url: "https://raw.githubusercontent.com/Rve27/beryllium/refs/heads/main/axion-changelogs.md" },
+	{ id: "horizon-beryllium-changelog", url: "https://raw.githubusercontent.com/Rve27/beryllium/refs/heads/main/horizon-changelogs.md" }
     ];
 
     changelogs.forEach(changelog => {
